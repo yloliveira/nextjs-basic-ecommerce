0cd63f7258ef99df199ce455409afb7f1ec174e1
Yan Oliveira <oliveira.yan02@gmail.com>
2024-08-27 15:37:52 -0300

    test (components/search): should call props.submit() when form is submitted

--- src/app/components/search.spec.tsx
+++ src/app/components/search.spec.tsx
@@ -1,9 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Search from "./search";
+
+const submit = jest.fn();
 
 describe("components/Search", () => {
   it("should render a form", async () => {
-    render(<Search />);
+    render(<Search submit={submit} />);
     expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+
+  it("should call props.submit() when form is submitted", async () => {
+    render(<Search submit={submit} />);
+    const form = screen.getByRole("form");
+
+    await fireEvent.submit(form);
+
+    expect(submit).toHaveBeenCalledTimes(1);
   });
 });
