ba67cb05b1896163f9af42bc87e0d9923205f55b
Yan Oliveira <oliveira.yan02@gmail.com>
2024-08-28 10:08:17 -0300

    test (components/product-card): should render free shipping text if the props.product.freeShipping is true

--- src/app/components/product-card.spec.tsx
+++ src/app/components/product-card.spec.tsx
@@ -6,25 +6,34 @@ const product = {
   price: 4990,
   image:
     "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0286e0143870775.6282940c27f00.jpeg",
+  freeShipping: true,
 };
 
-beforeEach(() => {
-  render(<ProductCard product={product} />);
-});
-
 describe("components/ProductCard", () => {
-  it("should render the props.product.image", async () => {
+  it("should render the props.product.image", () => {
+    render(<ProductCard product={product} />);
+
     expect(screen.getByRole("img")).toBeInTheDocument();
     expect(screen.getByRole("img")).toHaveProperty("alt", product.title);
   });
 
-  it("should render the props.product.title", async () => {
+  it("should render the props.product.title", () => {
+    render(<ProductCard product={product} />);
+
     expect(
       screen.getByText(new RegExp(product.title, "i"))
     ).toBeInTheDocument();
   });
 
-  it("should render the props.product.price", async () => {
+  it("should render the props.product.price", () => {
+    render(<ProductCard product={product} />);
+
     expect(screen.getByText(product.price)).toBeInTheDocument();
+  });
+
+  it("should render free shipping text if the props.product.freeShipping is true", () => {
+    render(<ProductCard product={product} />);
+
+    expect(screen.getByTestId("freeShipping")).toBeInTheDocument();
   });
 });
