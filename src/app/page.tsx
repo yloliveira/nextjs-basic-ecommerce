3e610678fe24f5f43500310b058d76fc3575d081
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-02 12:33:14 -0300

    feat: (page/home): should render the ProductList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 import React from "react";
+import ProductCard from "@/app/components/product-card";
 import Search from "@/app/components/search";
+import { Product } from "./models/product";
 
 export default function Home() {
+  const [products, setProducts] = React.useState<Product[]>([]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Search onSubmit={() => {}} />
+      <div data-testid="product-list">
+        {products.map(product => (
+          <ProductCard
+            onClick={() => {}}
+            product={product}
+            key={product.slugId}
+          />
+        ))}
+      </div>
     </main>
   );
 }
