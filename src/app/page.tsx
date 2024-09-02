95d80ae32abb1dceaf865fc6f89e0288d807aa7c
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-02 11:45:47 -0300

    feat: (page/home): should render Search form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import React from "react";
+import Search from "@/app/components/search";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+      <Search onSubmit={() => {}} />
     </main>
   );
 }
