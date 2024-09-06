67af5be35e909fa039d2c3a976c681aeb762a4aa
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-06 15:56:04 -0300

    feat (pages/home): should display the total quantity of products

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -11,14 +11,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Search onSubmit={() => {}} />
       <div data-testid="product-list">
-        {products.map(product => (
-          <ProductCard
-            onClick={() => {}}
-            product={product}
-            key={product.slugId}
-          />
-        ))}
-
         {products.length === 0 && !error && (
           <div data-testid="no-product">Nenhum Produto encontrado...</div>
         )}
@@ -26,6 +18,18 @@ export default function Home() {
         {error && (
           <div data-testid="fetch-error">Algo deu errado por aqui...</div>
         )}
+
+        {products.length > 0 && !error && (
+          <div data-testid="products-quantity">{products.length} produtos</div>
+        )}
+
+        {products.map(product => (
+          <ProductCard
+            onClick={() => {}}
+            product={product}
+            key={product.slugId}
+          />
+        ))}
       </div>
     </main>
   );
