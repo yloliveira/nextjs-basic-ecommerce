52a5b1ffcbfaf970dfe5c110df009f1c5f0b9a42
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-12 15:24:19 -0300

    test (hook/useFetchProducts): should be able to filter the result setting a filter

--- src/app/hooks/useFetchProducts.unit.spec.ts
+++ src/app/hooks/useFetchProducts.unit.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { Server, Response } from "miragejs";
 import { makeServer } from "@/mock-api/miragejs/server";
 import { useFetchProducts } from "./useFetchProducts";
@@ -20,6 +20,31 @@ describe("hooks/useFetchProducts", () => {
 
     await waitFor(() => {
       expect(result.current.products).toHaveLength(10);
+    });
+  });
+
+  it("should be able to filter the result setting a filter", async () => {
+    const searchTerm = "Product Title";
+    server.createList("product", 10);
+    server.create("product", {
+      title: searchTerm,
+    } as object);
+
+    const { result } = renderHook(() => useFetchProducts());
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(11);
+    });
+
+    act(() => {
+      result.current.setFilter(searchTerm);
+    });
+
+    await waitFor(() => {
+      expect(result.current.products).toHaveLength(1);
+      expect(result.current.products[0].title).toMatch(
+        new RegExp(searchTerm, "i")
+      );
     });
   });
 
