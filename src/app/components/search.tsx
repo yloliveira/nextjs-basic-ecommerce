b53a71f92a27880f428189817d12c0a96b5c7d45
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-12 11:30:26 -0300

    feat (components/search): should call props.submit() when search input is cleared

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -1,20 +1,29 @@
+import React from "react";
+
 type Props = {
   onSubmit: ({ text }: { text: string }) => void;
 };
 
 export default function Search({ onSubmit }: Props) {
+  const [text, setText] = React.useState("");
+
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const input = event.currentTarget.elements.namedItem(
-      "search-input"
-    ) as HTMLInputElement;
-
-    onSubmit({ text: input.value });
+    onSubmit({ text });
   }
+
+  const onInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setText(target.value);
+
+    if (target.value === "") {
+      onSubmit({ text: target.value });
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit} name="search-form">
-      <input type="search" name="search-input" />
+      <input type="search" name="search-input" onInput={onInput} />
     </form>
   );
 }
