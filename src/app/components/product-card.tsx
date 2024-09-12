65c41c27641d4b5017e376407a582297a109a2e3
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-12 15:15:46 -0300

    feat (components/ProductCard): add hover behavior

--- src/app/components/product-card.tsx
+++ src/app/components/product-card.tsx
@@ -8,9 +8,15 @@ type Props = {
 
 export default function ProductCard({ onClick, product }: Props) {
   return (
-    <div data-testid="product-card" onClick={() => onClick(product.slugId)}>
+    <div
+      data-testid="product-card"
+      onClick={() => onClick(product.slugId)}
+      className="cursor-pointer group"
+    >
       <img alt={product.title} src={product.image} className="w-full h-auto" />
-      <h3 className="text-sm my-3">{product.title}</h3>
+      <h3 className="text-sm my-3 group-hover:text-blue-500">
+        {product.title}
+      </h3>
       <h3 className="text-lg font-semibold">{product.price.originalAmount}</h3>
       {product.price.numberOfInstallmentsWithoutTaxes > 1 && (
         <div data-testid="installmentsText" className="text-xs font-light">
