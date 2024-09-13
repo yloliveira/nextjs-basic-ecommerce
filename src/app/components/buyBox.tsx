ce8e3d4505dd63c1ee2a79e6d06f48cb1c7dcd05
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-13 16:25:34 -0300

    feat (components/BuyBox): should render a 'Buy now' button

--- src/app/components/buyBox.tsx
+++ src/app/components/buyBox.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function BuyBox() {
-  return <div data-testid="buy-box"></div>;
+  return (
+    <div data-testid="buy-box">
+      <button data-testid="buy-now">Comprar agora</button>
+    </div>
+  );
 }
