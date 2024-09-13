3742730167ad9c12615a445b3427b4d3c8f1de3b
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-13 16:37:37 -0300

    feat (components/BuyBox): should render a quantity select box with 5 options

--- src/app/components/buyBox.tsx
+++ src/app/components/buyBox.tsx
@@ -3,6 +3,13 @@ import React from "react";
 export default function BuyBox() {
   return (
     <div data-testid="buy-box">
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
       <button data-testid="buy-now">Comprar agora</button>
       <button data-testid="add-to-cart">Adicionar ao carrinho</button>
     </div>
