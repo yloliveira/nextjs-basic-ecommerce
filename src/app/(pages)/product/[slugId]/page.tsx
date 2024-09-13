919a1f428d761750dd1dad4a7691570372590f56
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-13 16:53:05 -0300

    feat (pages/product): add page styles

--- src/app/(pages)/product/[slugId]/page.tsx
+++ src/app/(pages)/product/[slugId]/page.tsx
@@ -13,23 +13,29 @@ export default function Product({ params }: { params: { slugId: string } }) {
   }
 
   return (
-    <main className="bg-white w-full max-w-7xl p-5 shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 rounded-md mx-auto">
-      <img alt={product.title} src={product.image} className="w-full h-auto" />
-      <div>
-        <h1>{product.title}</h1>
-        <h3 data-testid="original-amount" className="text-lg font-semibold">
-          {Currency.format(product.price.originalAmount)}
-        </h3>
-        {product.price.numberOfInstallmentsWithoutTaxes > 1 && (
-          <div data-testid="installmentsText" className="text-xs font-light">
-            {`em ${
-              product.price.numberOfInstallmentsWithoutTaxes
-            }x ${Currency.format(product.price.installmentValue)} sem juros`}
-          </div>
-        )}
-      </div>
-      <BuyBox />
-      <p>{product.description}</p>
+    <main className="flex flex-col items-center justify-between gap-10 px-5">
+      <section className="bg-white w-full max-w-7xl p-5 shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 rounded-md">
+        <img
+          alt={product.title}
+          src={product.image}
+          className="w-full h-auto"
+        />
+        <div>
+          <h1>{product.title}</h1>
+          <h3 data-testid="original-amount" className="text-lg font-semibold">
+            {Currency.format(product.price.originalAmount)}
+          </h3>
+          {product.price.numberOfInstallmentsWithoutTaxes > 1 && (
+            <div data-testid="installmentsText" className="text-xs font-light">
+              {`em ${
+                product.price.numberOfInstallmentsWithoutTaxes
+              }x ${Currency.format(product.price.installmentValue)} sem juros`}
+            </div>
+          )}
+        </div>
+        <BuyBox />
+        <p>{product.description}</p>
+      </section>
     </main>
   );
 }
--- src/app/components/buyBox.tsx
+++ src/app/components/buyBox.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function BuyBox() {
   return (
-    <div data-testid="buy-box">
+    <div
+      data-testid="buy-box"
+      className="border rounded-lg border-gray-200 flex flex-col justify-between p-5 gap-2"
+    >
       <select>
         <option value="1">1</option>
         <option value="2">2</option>
@@ -10,8 +13,20 @@ export default function BuyBox() {
         <option value="4">4</option>
         <option value="5">5</option>
       </select>
-      <button data-testid="buy-now">Comprar agora</button>
-      <button data-testid="add-to-cart">Adicionar ao carrinho</button>
+      <div>
+        <button
+          data-testid="buy-now"
+          className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base"
+        >
+          Comprar agora
+        </button>
+        <button
+          data-testid="add-to-cart"
+          className="w-full h-12 rounded-md font-semibold text-base text-blue-500 bg-blue-50"
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
     </div>
   );
 }
