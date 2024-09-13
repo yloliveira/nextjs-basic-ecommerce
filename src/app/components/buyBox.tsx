98e9f554145c77b3af4cafd7f6d90747b7a74ee9
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-13 16:26:42 -0300

    feat (components/BuyBox): should render a 'Add to cart' button

--- src/app/components/buyBox.tsx
+++ src/app/components/buyBox.tsx
@@ -4,6 +4,7 @@ export default function BuyBox() {
   return (
     <div data-testid="buy-box">
       <button data-testid="buy-now">Comprar agora</button>
+      <button data-testid="add-to-cart">Adicionar ao carrinho</button>
     </div>
   );
 }
