325db1aa021c133ed590ae1cdcdedfdada110d99
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-19 15:34:24 -0300

    feat (stores/cart-store): should add 2 products to the list

--- src/app/stores/cart-store.ts
+++ src/app/stores/cart-store.ts
@@ -5,10 +5,21 @@ interface Props {
   state: {
     products: Product[];
   };
+  actions: {
+    add: (product: Product) => void;
+  };
 }
 
 export const useCartStore = create<Props>(set => ({
   state: {
     products: [],
+  },
+  actions: {
+    add: product =>
+      set(store => ({
+        state: {
+          products: [...store.state.products, product],
+        },
+      })),
   },
 }));
