fe886cc8d1902cc430889f23b8379f0dc07fbbaa
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-19 16:57:11 -0300

    test (stores/cart-store): should not add the same product twice

--- src/app/stores/cart-store.unit.spec.ts
+++ src/app/stores/cart-store.unit.spec.ts
@@ -2,10 +2,16 @@ import { renderHook, act } from "@testing-library/react";
 import { Server } from "miragejs";
 import { makeServer } from "@/mock-api/miragejs/server";
 import { Product } from "@/app/models/product";
-import { useCartStore } from "./cart-store";
+import { useCartStore, useCartStoreProps } from "./cart-store";
 
 describe("stores/cart-store", () => {
   let server: Server;
+  let store: useCartStoreProps;
+
+  beforeAll(() => {
+    const { result } = renderHook(() => useCartStore());
+    store = result.current;
+  });
 
   beforeEach(() => {
     server = makeServer();
@@ -13,6 +19,9 @@ describe("stores/cart-store", () => {
 
   afterEach(() => {
     server.shutdown();
+    act(() => {
+      store.actions.reset();
+    });
   });
 
   it("should return an empty array for products on initial state", () => {
@@ -35,5 +44,18 @@ describe("stores/cart-store", () => {
     }
 
     expect(result.current.state.products).toHaveLength(2);
+  });
+
+  it("should not add the same product twice", async () => {
+    const { result } = renderHook(() => useCartStore());
+    const product = server.create("product");
+    const { add } = result.current.actions;
+
+    expect(result.current.state.products).toHaveLength(0);
+
+    act(() => add(product.attrs as Product));
+    act(() => add(product.attrs as Product));
+
+    expect(result.current.state.products).toHaveLength(1);
   });
 });
