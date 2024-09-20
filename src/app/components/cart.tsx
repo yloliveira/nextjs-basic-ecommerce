752aa097e70058a9c2ab4bc6949b1d85a111c7a1
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-20 16:57:15 -0300

    feat (components/cart): add basic styles

--- src/app/components/cart.tsx
+++ src/app/components/cart.tsx
@@ -12,28 +12,35 @@ export default function Cart({
   onClickClose,
 }: Props) {
   return (
-    <div className="absolute" data-testid="cart-modal">
-      <div
-        data-testid="close"
-        className="cursor:pointer"
-        onClick={() => onClickClose()}
-      >
-        X
+    <div
+      className="absolute w-screen h-screen top-0 right-0 left-0 z-50 bg-opacity-50 bg-black flex justify-end"
+      data-testid="cart-modal"
+    >
+      <div className="w-full h-full max-w-md bg-white flex flex-col justify-between items-end p-5">
+        <div
+          data-testid="close"
+          className="cursor-pointer"
+          onClick={() => onClickClose()}
+        >
+          X
+        </div>
+        <div className="w-full">
+          <button
+            data-testid="see-more-products"
+            className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base"
+            onClick={() => onClickSeeMoreProducts()}
+          >
+            Ver mais produtos
+          </button>
+          <button
+            data-testid="checkout"
+            className="w-full h-12 rounded-md font-semibold text-base text-blue-500 bg-blue-50"
+            onClick={() => onClickCheckout()}
+          >
+            Finalizar a compra
+          </button>
+        </div>
       </div>
-      <button
-        data-testid="see-more-products"
-        className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base"
-        onClick={() => onClickSeeMoreProducts()}
-      >
-        Ver mais produtos
-      </button>
-      <button
-        data-testid="checkout"
-        className="w-full h-12 rounded-md font-semibold text-base text-blue-500 bg-blue-50"
-        onClick={() => onClickCheckout()}
-      >
-        Finalizar a compra
-      </button>
     </div>
   );
 }
