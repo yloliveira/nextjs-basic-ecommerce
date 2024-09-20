1c5141a0c07b7fe7b4fe6fab511bb00ed1c3dddb
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-20 16:13:03 -0300

    feat (components/cart): should render a 'checkout' button

---
+++ src/app/components/cart.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Cart() {
+  return (
+    <div className="" data-testid="cart-modal">
+      <button
+        data-testid="checkout"
+        className="w-full h-12 rounded-md font-semibold text-base text-blue-500 bg-blue-50"
+        onClick={() => {}}
+      >
+        Finalizar a compra
+      </button>
+    </div>
+  );
+}
