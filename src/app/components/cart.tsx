427b9647cd465c3353f24184f604e1bfdfa8add7
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-20 16:16:32 -0300

    feat (components/cart): should render a 'see more products' button

--- src/app/components/cart.tsx
+++ src/app/components/cart.tsx
@@ -4,6 +4,13 @@ export default function Cart() {
   return (
     <div className="" data-testid="cart-modal">
       <button
+        data-testid="see-more-products"
+        className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base"
+        onClick={() => {}}
+      >
+        Ver mais produtos
+      </button>
+      <button
         data-testid="checkout"
         className="w-full h-12 rounded-md font-semibold text-base text-blue-500 bg-blue-50"
         onClick={() => {}}
