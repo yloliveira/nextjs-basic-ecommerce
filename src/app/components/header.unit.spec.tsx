0e3fe8147358857cd1775997c786249ca642810e
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-23 15:21:46 -0300

    test (components/header): should render nav component

--- src/app/components/header.unit.spec.tsx
+++ src/app/components/header.unit.spec.tsx
@@ -29,6 +29,25 @@ describe("components/Header", () => {
     expect(screen.getByRole("form")).toHaveProperty("name", "search-form");
   });
 
+  it("should render nav component", () => {
+    render(<Header />);
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: /crie sua conta/i })
+    ).toHaveAttribute("href", "/registration");
+    expect(screen.getByRole("link", { name: /entre/i })).toHaveAttribute(
+      "href",
+      "/login"
+    );
+    expect(screen.getByRole("link", { name: /compras/i })).toHaveAttribute(
+      "href",
+      "/purchases"
+    );
+    expect(
+      screen.getByRole("link", { description: /carrinho/i })
+    ).toHaveAttribute("href", "/cart");
+  });
+
   it("should call router.push() with the url containing the correct term for search", async () => {
     const searchTerm = "Product Title";
 
