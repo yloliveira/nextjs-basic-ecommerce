771fb5581909f1e6d3e8959f7a1eabdb46ce7927
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-23 15:44:35 -0300

    feat (components/header): should render nav component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -11,10 +11,58 @@ export default function Header() {
 
   return (
     <header
-      className="w-full h-28 bg-brandPrimary py-2 px-5 sm:mb-10"
+      className="w-full h-28 bg-brandPrimary py-2 px-5 sm:mb-10 flex justify-center items-center"
       data-testid="header"
     >
-      <Search onSubmit={onSubmitSearchForm} />
+      <div className="w-full max-w-7xl h-full flex flex-col justify-between">
+        <Search onSubmit={onSubmitSearchForm} />
+        <nav className="w-full h-8 flex justify-end items-center px-5">
+          <ul className="flex gap-5 text-sm">
+            <li>
+              <a href="/registration" role="link">
+                Crie sua conta
+              </a>
+            </li>
+            <li>
+              <a href="/login" role="link">
+                Entre
+              </a>
+            </li>
+            <li>
+              <a href="/purchases" role="link">
+                Compras
+              </a>
+            </li>
+          </ul>
+          <a
+            href="/cart"
+            role="link"
+            className="ml-5"
+            aria-describedby="carrinho"
+          >
+            <span hidden id="carrinho">
+              Carrinho
+            </span>
+            <svg
+              className="w-[25px] h-[25px] text-black"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1"
+                d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
+              />
+            </svg>
+          </a>
+        </nav>
+      </div>
     </header>
   );
 }
