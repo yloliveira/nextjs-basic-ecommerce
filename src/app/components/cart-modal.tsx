a6fd790a3362b2daf7024641e2adcb17abe42aeb
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-23 13:55:30 -0300

    fix (components/cart-modal): svg Invalid DOM property errors

--- src/app/components/cart-modal.tsx
+++ src/app/components/cart-modal.tsx
@@ -43,9 +43,9 @@ export default function CartModal({
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
             />
           </svg>
