1a871ed6ed74e3b0a9ac8b29774c54f00d7d0936
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-23 14:03:35 -0300

    test (page/product): should call router.push('/login'), when CheckoutButton is clicked, if there's no session_id into the sessionStorage

--- src/app/(pages)/product/[slugId]/product.page.integration.spec.tsx
+++ src/app/(pages)/product/[slugId]/product.page.integration.spec.tsx
@@ -139,6 +139,30 @@ describe("pages/Product", () => {
     });
   });
 
+  it("should call router.push('/login'), when CheckoutButton is clicked, if there's no session_id into the sessionStorage", async () => {
+    product = server.create("product").attrs as ProductModel;
+    render(<Product params={{ slugId: product.slugId }} />);
+
+    sessionStorage.setItem("session_id", "");
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByTestId("checkout"));
+      expect(nextNavigationPushMock).toHaveBeenCalledWith("/login");
+    });
+  });
+
+  it("should not call router.push('/login'), when CheckoutButton is clicked, if there's session_id into the sessionStorage", async () => {
+    product = server.create("product").attrs as ProductModel;
+    render(<Product params={{ slugId: product.slugId }} />);
+
+    sessionStorage.setItem("session_id", "valid_session_id");
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByTestId("checkout"));
+      expect(nextNavigationPushMock).not.toHaveBeenCalledWith("/login");
+    });
+  });
+
   it("should call router.push('/') when SeeMoreProductsButton is clicked", async () => {
     product = server.create("product").attrs as ProductModel;
     render(<Product params={{ slugId: product.slugId }} />);
