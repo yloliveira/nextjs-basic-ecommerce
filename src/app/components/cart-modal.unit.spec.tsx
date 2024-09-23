14a2005d3e2ec29c8bb5914739922fb4ec77a13e
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-23 12:47:12 -0300

    test (components/cart-modal): should have a hidden css class if props.isOpen is false

--- src/app/components/cart-modal.unit.spec.tsx
+++ src/app/components/cart-modal.unit.spec.tsx
@@ -5,51 +5,64 @@ const onClickSeeMoreProducts = jest.fn();
 const onClickCheckout = jest.fn();
 const onClickClose = jest.fn();
 
-describe("components/CartModal", () => {
-  beforeEach(() => {
-    render(
-      <CartModal
-        isOpen
-        onClickSeeMoreProducts={onClickSeeMoreProducts}
-        onClickCheckout={onClickCheckout}
-        onClickClose={onClickClose}
-      />
-    );
-  });
+const makeSut = (isOpen = true) => {
+  render(
+    <CartModal
+      isOpen={isOpen}
+      onClickSeeMoreProducts={onClickSeeMoreProducts}
+      onClickCheckout={onClickCheckout}
+      onClickClose={onClickClose}
+    />
+  );
+};
 
+describe("components/CartModal", () => {
   it("should render a close button", async () => {
+    makeSut();
     expect(screen.getByTestId("close")).toBeInTheDocument();
   });
 
   it("should render a 'see more products' button", async () => {
+    makeSut();
     expect(screen.getByTestId("see-more-products")).toBeInTheDocument();
   });
 
   it("should render a 'checkout' button", async () => {
+    makeSut();
     expect(screen.getByTestId("checkout")).toBeInTheDocument();
   });
 
   it("should have a absolute css class", () => {
+    makeSut();
     expect(screen.getByTestId("cart-modal")).toHaveClass("absolute");
   });
 
-  it("should display the content if props.isOpen is true", () => {
-    expect(screen.getByTestId("cart-modal")).toBeInTheDocument();
+  it("should not have a hidden css class if props.isOpen is true", () => {
+    makeSut();
+    expect(screen.getByTestId("cart-modal")).not.toHaveClass("hidden");
+  });
+
+  it("should have a hidden css class if props.isOpen is false", () => {
+    makeSut(false);
+    expect(screen.getByTestId("cart-modal")).toHaveClass("hidden");
   });
 
   it("should call props.onClickSeeMoreProducts()", () => {
+    makeSut();
     const [seeMoreProductsButton] = screen.getAllByRole("button");
     fireEvent.click(seeMoreProductsButton);
     expect(onClickSeeMoreProducts).toHaveBeenCalledTimes(1);
   });
 
   it("should call props.onClickCheckout()", () => {
+    makeSut();
     const [_, checkoutButton] = screen.getAllByRole("button");
     fireEvent.click(checkoutButton);
     expect(onClickCheckout).toHaveBeenCalledTimes(1);
   });
 
   it("should call props.onClickClose()", () => {
+    makeSut();
     const closeButton = screen.getByTestId("close");
     fireEvent.click(closeButton);
     expect(onClickClose).toHaveBeenCalledTimes(1);
