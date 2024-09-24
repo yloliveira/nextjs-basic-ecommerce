f1197654ac6326364a88e7c4c95646c9ccd48b30
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-24 15:06:18 -0300

    feat (pages/cart): should render the cart item component for each item inside the cart

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -1,13 +1,25 @@
 "use client";
+import { Product } from "@/app/models/product";
+import { useCartStore } from "@/app/stores/cart-store";
 import React from "react";
 
+function CartItem({ item }: { item: { product: Product; quantity: number } }) {
+  return <div data-testid="cart-item"></div>;
+}
+
 export default function Cart() {
+  const cartItems = useCartStore(state => state.state.items);
+
   return (
     <main className="w-full max-w-7xl grid grid-cols-7 mx-auto mt-5 md:mt-0 gap-5 lg:px-5">
       <section
         data-testid="cart-items-list"
         className="bg-white p-5 shadow col-span-7 lg:col-span-5 rounded-md mx-5 lg:mx-0"
-      ></section>
+      >
+        {cartItems.map(item => (
+          <CartItem item={item} key={item.product.slugId} />
+        ))}
+      </section>
       <section
         data-testid="purchase-summary"
         className="bg-white p-5 shadow col-span-7 lg:col-span-2 lg:rounded-md"
