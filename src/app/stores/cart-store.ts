7f8717807565f2251187eef02c7ddf8fe163ef61
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-24 15:41:16 -0300

    feat (stores/cart-store): should persist the cart data into the session storage

--- src/app/stores/cart-store.ts
+++ src/app/stores/cart-store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { Product } from "../models/product";
 
 export type Toggle = () => void;
@@ -38,52 +39,58 @@ const initialState = {
 };
 
 export const useCartStore = create<useCartStoreProps>()(
-  immer(set => ({
-    state: { ...initialState },
-    actions: {
-      toggle() {
-        set(({ state }) => {
-          state.open = !state.open;
-        });
-      },
-      add({ product, quantity }) {
-        set(({ state }) => {
-          const productIndex = state.items.findIndex(
-            (item: CartItem) => item.product.slugId === product.slugId
-          );
-          const PRODUCT_NOT_ADDED = productIndex < 0;
-          if (PRODUCT_NOT_ADDED) {
-            state.items.push({ product, quantity });
-          } else {
-            state.items[productIndex].quantity += quantity;
-          }
-        });
-      },
-      remove(product) {
-        set(({ state }) => {
-          const productIndex = state.items.findIndex(
-            (item: CartItem) => item.product.slugId === product.slugId
-          );
-          const PRODUCT_FOUND = productIndex >= 0;
-          if (PRODUCT_FOUND) {
-            if (state.items[productIndex].quantity > 1) {
-              state.items[productIndex].quantity -= 1;
+  persist(
+    immer(set => ({
+      state: { ...initialState },
+      actions: {
+        toggle() {
+          set(({ state }) => {
+            state.open = !state.open;
+          });
+        },
+        add({ product, quantity }) {
+          set(({ state }) => {
+            const productIndex = state.items.findIndex(
+              (item: CartItem) => item.product.slugId === product.slugId
+            );
+            const PRODUCT_NOT_ADDED = productIndex < 0;
+            if (PRODUCT_NOT_ADDED) {
+              state.items.push({ product, quantity });
             } else {
-              state.items.splice(productIndex, 1);
+              state.items[productIndex].quantity += quantity;
             }
-          }
-        });
+          });
+        },
+        remove(product) {
+          set(({ state }) => {
+            const productIndex = state.items.findIndex(
+              (item: CartItem) => item.product.slugId === product.slugId
+            );
+            const PRODUCT_FOUND = productIndex >= 0;
+            if (PRODUCT_FOUND) {
+              if (state.items[productIndex].quantity > 1) {
+                state.items[productIndex].quantity -= 1;
+              } else {
+                state.items.splice(productIndex, 1);
+              }
+            }
+          });
+        },
+        removeAll() {
+          set(({ state }) => {
+            state.items = [];
+          });
+        },
+        reset() {
+          set(store => {
+            store.state = initialState;
+          });
+        },
       },
-      removeAll() {
-        set(({ state }) => {
-          state.items = [];
-        });
-      },
-      reset() {
-        set(store => {
-          store.state = initialState;
-        });
-      },
-    },
-  }))
+    })),
+    {
+      name: "cart-store",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
 );
