c9129f8e32156f9d8e99ee9fc28f6d821347c7f4
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-27 16:15:25 -0300

    feat (pages/cart): should render a quantity stepper

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -7,15 +7,21 @@ import Currency from "@/app/utils/currency";
 
 function CartItem({ item }: { item: { product: Product; quantity: number } }) {
   return (
-    <div data-testid="cart-item" className="flex gap-5">
-      <img
-        alt={item.product.title}
-        src={item.product.image}
-        className="w-16 h-16"
-      />
-      <div className="w-full flex justify-between items-center">
+    <div
+      data-testid="cart-item"
+      className="grid grid-cols-1 sm:grid-cols-2 gap-5 border-b p-5 border-gray-200"
+    >
+      <div className="flex gap-5">
+        <img
+          alt={item.product.title}
+          src={item.product.image}
+          className="w-16 h-16"
+        />
+
         <div className="h-full flex flex-col justify-between items-start">
-          <h3 className="text-base font-semibold">{item.product.title}</h3>
+          <h3 className="text-base font-semibold line-clamp-1">
+            {item.product.title}
+          </h3>
           <button
             onClick={() => {}}
             className="font-semibold text-sm text-blue-500"
@@ -23,7 +29,62 @@ function CartItem({ item }: { item: { product: Product; quantity: number } }) {
             Excluir
           </button>
         </div>
-        <span data-testId="item-total" className="text-xl font-medium">
+      </div>
+
+      <div className="w-full flex justify-between items-start">
+        <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2">
+          <div
+            data-testid="quantity-stepper"
+            className="border rounded-md border-gray-200 px-3 h-[32px] min-w-[70px] max-w-[90px] sm:max-w-[120px] flex gap-6 items-center justify-center"
+          >
+            <button
+              onClick={() => {}}
+              data-testid="decrease-quantity"
+              className="text-blue-500 font-bold text-3xl"
+            >
+              <svg
+                className="w-[16px] h-[16px] text-blue-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h14"
+                />
+              </svg>
+            </button>
+            <span data-testid="item-quantity" className="text-sm">
+              {item.quantity}
+            </span>
+            <button data-testid="increase-quantity" onClick={() => {}}>
+              <svg
+                className="w-[16px] h-[16px] text-blue-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h14m-7 7V5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <span data-testid="item-total" className="text-xl font-medium">
           {Currency.format(item.product.price.originalAmount * item.quantity)}
         </span>
       </div>
@@ -44,43 +105,54 @@ export default function Cart() {
     <main className="w-full max-w-7xl grid grid-cols-7 mx-auto mt-5 md:mt-0 gap-5 lg:px-5">
       <section
         data-testid="cart-items-list"
-        className="flex flex-col gap-5 bg-white p-5 shadow col-span-7 lg:col-span-5 rounded-md mx-5 lg:mx-0"
+        className="flex flex-col bg-white shadow col-span-7 lg:col-span-5 rounded-md mx-5 lg:mx-0"
       >
+        <h1 className="p-5 text-lg font-bold border-b border-gray-200">
+          Produtos
+        </h1>
         {cartItems.map(item => (
           <CartItem item={item} key={item.product.slugId} />
         ))}
       </section>
       <section
         data-testid="purchase-summary"
-        className="block overflow-auto bg-white p-5 shadow col-span-7 lg:col-span-2 lg:rounded-md flex flex-col gap-3"
+        className="block overflow-auto bg-white shadow col-span-7 lg:col-span-2 lg:rounded-md"
+        style={{
+          height: "fit-content",
+        }}
       >
-        <div
-          data-testid="products-total"
-          className="flex justify-between text-sm  font-light"
-        >
-          <span>{`Produtos(${cartItems.length})`} </span>
-          <span>{Currency.format(productsTotal)}</span>
+        <h1 className="p-5 text-lg font-bold border-b border-gray-200">
+          Resumo da compra
+        </h1>
+        <div className="p-5 flex flex-col gap-3">
+          <div
+            data-testid="products-total"
+            className="flex justify-between text-sm font-light"
+          >
+            <span>{`Produtos(${cartItems.length})`} </span>
+            <span>{Currency.format(productsTotal)}</span>
+          </div>
+          <div
+            data-testid="shipping"
+            className="flex justify-between text-sm font-light"
+          >
+            <span>frete </span>
+            <span>{Currency.format(shipping)}</span>
+          </div>
+          <div
+            data-testid="total"
+            className="flex justify-between font-semibold text-lg  font-light"
+          >
+            <span>Total </span>
+            <span>{Currency.format(total)}</span>
+          </div>
+          <button
+            className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base mb-2"
+            onClick={() => {}}
+          >
+            Continuar a compra
+          </button>
         </div>
-        <div
-          data-testid="shipping"
-          className="flex justify-between text-sm font-light"
-        >
-          <span>frete </span>
-          <span>{Currency.format(shipping)}</span>
-        </div>
-        <div
-          data-testid="total"
-          className="flex justify-between font-semibold text-lg  font-light"
-        >
-          <span>Total </span>
-          <span>{Currency.format(total)}</span>
-        </div>
-        <button
-          className="w-full h-12 bg-blue-500 rounded-md text-white font-semibold text-base mb-2"
-          onClick={() => {}}
-        >
-          Continuar a compra
-        </button>
       </section>
     </main>
   );
