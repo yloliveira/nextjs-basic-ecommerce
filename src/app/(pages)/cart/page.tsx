c31986d8fa3cda9d0c2cb0d712ec845b7c58c557
Yan Oliveira <oliveira.yan02@gmail.com>
2024-09-30 11:24:38 -0300

    feat (pages/cart): should increase the item quantity by 1 when increaseButton is clicked

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -109,7 +109,7 @@ export function CartItem({
 
 export default function Cart() {
   const cartItems = useCartStore(state => state.state.items);
-  const { remove } = useCartStore(state => state.actions);
+  const { add, remove } = useCartStore(state => state.actions);
   const productsTotal = cartItems.reduce((acc, cur) => {
     acc += cur.quantity * cur.product.price.originalAmount;
     return acc;
@@ -125,7 +125,9 @@ export default function Cart() {
     remove({ product, quantity: 1 });
   };
 
-  const onClickIncrease = () => {};
+  const onClickIncrease = (product: Product) => {
+    add({ product, quantity: 1 });
+  };
 
   return (
     <main className="w-full max-w-7xl grid grid-cols-7 mx-auto mt-5 md:mt-0 gap-5 lg:px-5">
