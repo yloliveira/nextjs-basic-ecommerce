acd7b9ff9fa049e2e0e8fb3e7609eac6cc1a2e0f
Yan Oliveira <oliveira.yan02@gmail.com>
2024-10-01 15:46:28 -0300

    feat (plugins/miragejs): should return the server only if the environment is development

--- src/app/plugins/miragejs.ts
+++ src/app/plugins/miragejs.ts
@@ -1,5 +1,14 @@
-if (process.env.NODE_ENV === "development") {
-  require("../../mock-api/miragejs/server").makeServer({
-    environment: "development",
-  });
+import { Server } from "miragejs";
+
+export default function init(): Server | undefined {
+  if (process.env.NODE_ENV === "development") {
+    const server = require("../../mock-api/miragejs/server").makeServer({
+      environment: "development",
+    });
+
+    return server as Server;
+  }
+  return undefined;
 }
+
+init();
