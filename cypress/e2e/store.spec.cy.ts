400b9e25ba96172ef20afc9ddd92b49148ff4ec8
Yan Oliveira <oliveira.yan02@gmail.com>
2024-10-01 11:46:58 -0300

    test (e2e/home): should type in the search form

--- cypress/e2e/store.spec.cy.ts
+++ cypress/e2e/store.spec.cy.ts
@@ -1,8 +1,20 @@
-describe("Store", () => {
+/// <reference types="cypress" />
+
+context("Store", () => {
   it("should display the store", () => {
     cy.visit("/");
 
     cy.get("body").contains("mercado preso");
     cy.get("body").contains("Ofertas do dia");
+  });
+
+  context("Search for products", () => {
+    it("should type in the search form", () => {
+      cy.visit("/");
+
+      cy.get("input[type='search']")
+        .type("Some text")
+        .should("have.value", "Some text");
+    });
   });
 });
